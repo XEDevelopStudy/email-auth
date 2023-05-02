e8d65964000c2c1e2331177621fb93480dbaa630
wnguds <wnguds1101@gmail.com>
2023-05-02 17:30:30 +0900

    Initial commit

---
+++ src/main/java/com/wnguds/email/emailauth/EmailAuthApplication.java
@@ -0,0 +1,13 @@
+package com.wnguds.email.emailauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wnguds/email/emailauth/EmailAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnguds.email.emailauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
